f4d7d7d089b513d5f239177df1078da324517db0
Daniel <danialups1997@gmail.com>
2020-10-03 23:59:58 +0200

    css is done, hey this commit messages are not for you so don't whine LOL

--- day-2/app.js
+++ day-2/app.js
@@ -1,2 +1,46 @@
+window.onload = function () {
+    var generate = document.getElementById('generate-btn');
+    // i couldn't come up with anything else LOL
+    var quoteOnThePage = document.getElementById('quote');
+    const quotes = [
+        {
+            quote:
+                "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
+            author: " Life",
+        },
+        {
+            quote:
+                "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
+            author: "John Kenneth Galbraith",
+        },
+        {
+            quote:
+                "God save me from my friends. I can protect myself from my enemies.",
+            author: "Claude Louis Hector de Villars ",
+        },
+        {
+            quote:
+                "The price of anything is the amount of life you exchange for it.",
+            author: "David Thoreau",
+        },
+        {
+            quote:
+                "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
+            author: "Charles Lindbergh",
+        },
+        {
+            quote:
+                "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
+            author: " Tyne Daly",
+        },
+    ];
 
-console.log('daniel');
+    function randomQuote() {
+        return Math.floor(Math.random() * 6);
+    }
+
+    generate.addEventListener('click', function () {
+        quoteOnThePage.innerHTML = quotes[randomQuote()]['quote'];
+    });
+
+};
