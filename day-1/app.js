31e4778580fdafd94731d29b38103da61f0366cf
Daniel <danialups1997@gmail.com>
2020-10-03 18:56:42 +0200

    project one complete

--- day-1/app.js
+++ day-1/app.js
@@ -1,8 +1,9 @@
 var push = document.getElementById("pushMe");
+var hex = document.getElementById('hex');
 
 function generateRandmoColor() {
     var letter = "0123456789abcdef";
-    
+
     var hex = "#";
     for (var i = 0; i < 6; i++) {
         hex += letter[Math.floor(Math.random() * 16)];
@@ -10,8 +11,12 @@ function generateRandmoColor() {
     return hex;
 }
 
+// daniel was here from window
 push.addEventListener("click", function (e) {
     e.preventDefault();
     var res = generateRandmoColor();
+    hex.innerHTML = 'The hex is: ' + res;
     document.body.style.backgroundColor = res;
 });
+
+
